af1a81d7f515bd4235fd0e83a0cb0463d702cc28
happiguru <hguruman@gmail.com>
2020-09-02 23:06:02 +0400

    Added sql for select without clauses

--- sql_zoo.sql
+++ sql_zoo.sql
@@ -8,4 +8,29 @@ CREATE TABLE world(
   area NUMERIC,
   c_population NUMERIC,
   gdp NUMERIC
-)
+);
+
+/* number one */
+SELECT population FROM world
+  WHERE name = 'Germany';
+
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
+
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000;
+
+SELECT name, population
+  FROM world
+ WHERE population BETWEEN 1000000 AND 1250000;
+
+ SELECT name, population
+      FROM world
+      WHERE name LIKE "Al%";
+
+SELECT name FROM world
+ WHERE name LIKE '%a' OR name LIKE '%l';
+
+SELECT name,length(name)
+FROM world
+WHERE length(name)=5 and region='Europe';
