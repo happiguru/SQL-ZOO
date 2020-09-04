b81b1d4c84c5f79af2a4f66342b72697aab3c078
happiguru <hguruman@gmail.com>
2020-09-04 17:11:36 +0400

    Add select from nobel

---
+++ select_from_noble.sql
@@ -0,0 +1,63 @@
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
+
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+
+SELECT yr, subject
+  FROM nobel
+ WHERE winner = 'Albert Einstein';
+
+SELECT winner
+  FROM  nobel
+ WHERE yr > 1999 AND subject = 'Peace';
+ SELECT winner
+  FROM  nobel
+ WHERE yr >= 2000 AND subject = 'Peace';
+
+SELECT * 
+  FROM nobel
+ WHERE subject = 'Literature' AND yr BETWEEN 1980 and 1989;
+
+SELECT * FROM nobel
+ WHERE subject = 'Peace'
+  AND winner IN ('Barack Obama',
+                  'Jimmy Carter',
+                  'Woodrow Wilson',
+                  'Theodore Roosevelt');
+
+SELECT winner
+  FROM nobel 
+ WHERE winner LIKE 'John%';
+
+SELECT * FROM nobel
+WHERE subject = 'Physics'
+AND yr = 1980
+OR subject = 'Chemistry'
+AND yr = 1984;
+
+SELECT * 
+  FROM nobel 
+ WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine');
+
+SELECT *
+  FROM nobel
+ WHERE subject = 'Medicine' AND yr < 1910 
+OR subject = 'Literature' and yr >= 2004;
+
+SELECT *
+  FROM nobel
+ WHERE winner = 'PETER GRÜNBERG';
+
+SELECT * 
+  FROM nobel 
+ WHERE winner = 'EUGENE O''NEILL';
+
+SELECT winner, yr, subject 
+  FROM nobel 
+WHERE winner LIKE 'Sir%'
+ORDER BY yr DESC;
+
--- select_name.sql
+++ select_name.sql
@@ -26,5 +26,27 @@ ORDER BY name;
 SELECT name FROM world
  WHERE name LIKE '%o__o%';
 
+-- Cuba and Togo have four characters names.
+
+-- Find the countries that have exactly four characters.
 SELECT name FROM world
  WHERE name LIKE '____';
+
+-- 11. The capital of Luxembourg is Luxembourg. Show all the countries where the capital is the same as the name of the country
+
+-- Find the country where the name is the capital city.
+
+SELECT name
+  FROM world
+ WHERE name LIKE capital
+
+SELECT name
+  FROM world
+ WHERE capital LIKE concat(name, ' City');
+
+SELECT capital, name 
+  FROM world 
+ WHERE capital LIKE concat('%', name, '%');
+
+SELECT capital, name FROM world WHERE capital LIKE concat(name,'_%');
+
