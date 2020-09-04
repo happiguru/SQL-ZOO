3d1d2efa51f168d8afeec760b6e1efa71838f93d
happiguru <hguruman@gmail.com>
2020-09-04 17:17:05 +0400

    Add select from nobel

--- select_from_noble.sql
+++ select_from_noble.sql
@@ -61,3 +61,8 @@ SELECT winner, yr, subject
 WHERE winner LIKE 'Sir%'
 ORDER BY yr DESC;
 
+SELECT winner, subject
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY subject IN ('Physics','Chemistry'),subject,winner;
+
