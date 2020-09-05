c35d5987980e0c021f13e9b9cbc6f7156f818714
Stanley Enow Lekunze <hguruman@gmail.com>
2020-09-05 17:00:10 +0400

    Added lesson 8 and 9

---
+++ self_join.sql
@@ -0,0 +1,16 @@
+
+SELECT count(*) FROM stops
+
+SELECT id FROM stops
+WHERE name = 'Craiglockhart'
+
+SELECT stops.id, name FROM stops
+JOIN route
+ON stops.id = route.stop
+where num = 4 AND company = 'LRT'
+ORDER BY pos
+
+SELECT company, num, COUNT(*)
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num
+HAVING COUNT(*)>= 2
---
+++ windows.sql
@@ -0,0 +1,5 @@
+SELECT lastName, party, votes
+  FROM ge
+ WHERE constituency = 'S14000024' AND yr = 2017
+ORDER BY votes DESC;
+
