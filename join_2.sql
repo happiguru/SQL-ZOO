7ff2b074c33a2319316deb7e8194d2d20a571ebc
Stanley Enow Lekunze <hguruman@gmail.com>
2020-09-05 07:45:41 +0400

    Added join2 file

---
+++ join_2.sql
@@ -0,0 +1,21 @@
+SELECT id, title
+ FROM movie
+ WHERE yr=1962;
+
+SELECT yr
+  FROM movie
+  WHERE title = 'Citizen Kane';
+
+SELECT id, title, yr
+FROM movie
+WHERE title LIKE '%Star Trek%'
+ORDER BY yr;
+
+SELECT id
+FROM actor
+WHERE name = 'Glenn Close';
+
+SELECT id
+FROM movie
+WHERE title = 'Casablanca';
+
